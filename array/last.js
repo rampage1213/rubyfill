d65ef2bf990e3b4ee326abdd1d7640246e6e2ea0
Quintin <quintinreineccius@gmail.com>
2015-09-18 00:35:10 -0700

    small fix

--- array/last.js
+++ array/last.js
@@ -1,7 +1,7 @@
 if (!Array.prototype.last) {
   Object.defineProperty(Array.prototype, 'last', {
     value: function(num) {
-      var n = num ? num : 1
+      var n = num ? num : 1;
       return this.splice(this.length - n, this.length);
     }
   });
