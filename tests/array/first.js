5033dbf145082ca8152c051a7c01583087434226
Quintin <quintinreineccius@gmail.com>
2015-09-17 20:24:06 -0700

    Improved .first tests

--- tests/array/first.js
+++ tests/array/first.js
@@ -5,11 +5,14 @@ require('../../array/first');
 
 describe('first', function() {
   it('should grab the first item from the array', function() {
-    expect(["t", "e", "s", "t"].first(1)).to.eql(["t"]);
-    expect(["t", "e", "s", "t"].first(2)).to.eql(["t", "e"]);
+    var array = ["t", "e", "s", "t"];
+
+    expect(array.first()).to.eql(["t"]);
   });
 
-  it('should grab the first items from the array depending on what number was passed through', function() {
-    expect(["t", "e", "s", "t"].first(2)).to.eql(["t", "e"]);
+  it('should grab the first two items from the array', function() {
+    var array = ["t", "e", "s", "t"];
+
+    expect(array.first(2)).to.eql(["t", "e"]);
   });
 });
