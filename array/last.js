b4fc1911e667fdbb2fcd3a297ca148ba2bc6318b
Quintin <quintinreineccius@gmail.com>
2015-09-17 23:39:36 -0700

    changed .last() to a ternary operator instead of a if statement

--- array/last.js
+++ array/last.js
@@ -1,12 +1,8 @@
 if (!Array.prototype.last) {
   Object.defineProperty(Array.prototype, 'last', {
     value: function(num) {
-      if(num){
-         return this.splice(this.length - num, this.length);
-      }
-      else{
-        return this.splice(this.length - 1, this.length);
-      }
-    } 
+      var n = num ? num : 1
+      return this.splice(this.length - n, this.length);
+    }
   });
 }
